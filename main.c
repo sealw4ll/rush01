c89b63e3ff5e002d51f53f117741fe0e57641669
seal <wting@u81z03s03.42kl.edu.my>
2022-04-16 16:45:46 +0800

    boxsize2

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+int	ft_boxsize(char *input);
+
 int main(int argc, char *argv[])
 {
 	if (argc != 2)
@@ -12,6 +14,6 @@ int main(int argc, char *argv[])
 	char	*input = argv[1];
 	printf("%d\n", argc);
 	printf("%s\n", input);	
-	printf("%d\n", sizeof(*input));
+	printf("%d\n", ft_boxsize(input));
 	return 0;
 }
